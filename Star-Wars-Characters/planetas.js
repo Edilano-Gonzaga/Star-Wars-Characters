3b52490c95930f256dcfa226376a5457005ab66c
Edilano Gonzaga <edilanog1@gmail.com>
2023-06-27 21:49:08 -0300

    Add files via upload

---
+++ Star-Wars-Characters/planetas.js
@@ -0,0 +1,185 @@
+let currentPageUrl = 'https://swapi.dev/api/planets/' //Foi usado Let porque esse valor da variavel vai ficar mudando, let e var pode ser redeclarado o const não pode é um valor constante, pega a visão não esquecer disso.//
+
+window.onload = async () => {
+    try{
+         await loadCharacters(currentPageUrl);
+    } catch (error) {
+        console.log(error);
+        alert('error ao carregar cards');
+    }
+
+    const nextButton = document.getElementById('next-button')
+    const backButton = document.getElementById('back-button')
+
+    nextButton.addEventListener('click', loadNextPage)
+    backButton.addEventListener('click', loadPreviousPage)
+
+};
+
+async function loadCharacters(url) {
+    const mainContent = document.getElementById('main-content')
+    mainContent.innerHTML = ''; // Limpar os resultados anteriores.
+
+    try {
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((character) => {
+            const card = document.createElement("div")
+            card.style.backgroundImage = 
+            `url('https://starwars-visualguide.com/assets/img/planets/${character.url.replace(/\D/g, "")}.jpg')`
+            card.className = "cards"
+          
+
+            const characterNameBG = document.createElement("div")
+            characterNameBG.className = "character-name-bg"
+
+            const characterName = document.createElement("span")
+            characterName.className = "character-name"
+            characterName.innerText = `${character.name}`
+
+            characterNameBG.appendChild(characterName) // insira um elemento dentro desse elemento ou seja, pega um elemento filho e coloca dentro do elemento pai, nesse caso o charactername é filho do characternameBG//
+            card.appendChild(characterNameBG)
+
+            card.onclick = () =>{
+                const modal = document.getElementById("modal")
+                modal.style.visibility = "visible"
+
+                const modalContent = document.getElementById("modal-content")
+                modalContent.innerHTML = ''
+
+                const characterImage = document.createElement("div")
+                characterImage.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/planets/${character.url.replace(/\D/g, "")}.jpg'`
+                characterImage.className = "character-image"
+
+                const name = document.createElement("span")
+                name.className = "character-details"
+                name.innerText = `Nome: ${character.name}`
+
+                const characterDiameter = document.createElement("span")
+                characterDiameter.className = "character-details"
+                characterDiameter.innerText = `Diametro: ${character.diameter}`
+
+                const rotation_period = document.createElement("span")
+                rotation_period.className = "character-details"
+                rotation_period.innerText = `Periodo_Rotacao: ${character.rotation_period}`
+
+                const climate = document.createElement("span")
+                climate.className = "character-details"
+                climate.innerText = `Clima: ${character.climate}`
+
+                const population = document.createElement("span")
+                population.className = "character-details"
+                population.innerText = `Populacao: ${character.population}`
+
+                modalContent.appendChild(characterImage)
+                modalContent.appendChild(name)
+                modalContent.appendChild(characterDiameter)
+                modalContent.appendChild(rotation_period)
+                modalContent.appendChild(climate)
+                modalContent.appendChild(population)
+            }
+
+          
+            
+
+            
+            mainContent.appendChild(card)
+        });
+
+        const nextButton = document.getElementById("next-button")
+        const backButton = document.getElementById("back-button")
+
+        nextButton.disabled = !responseJson.next
+        backButton.disabled = !responseJson.previous
+
+        backButton.style.visibility = responseJson.previous? "visible" : "hidden"
+
+        currentPageUrl = url
+
+    } catch(error) {
+        alert('Erro ao carregar os personagens')
+        console.log(error)
+    }
+}
+
+async function loadNextPage() {
+    if (!currentPageUrl) return;
+
+    try {
+
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.next)
+
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a próxima página')
+    }
+
+}
+
+async function loadPreviousPage(){
+    if (!currentPageUrl) return;
+
+    try{
+
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.previous)
+
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a página anterior')
+    }
+
+}
+
+function hidenModal (){
+    const modal = document.getElementById("modal")
+    modal.style.visibility = "hidden"
+}
+
+// function convertEyecolor(eyeColor) {
+//     const cores = {
+//         blue: "azul",
+//         brown: "castanho",
+//         green: "verde",
+//         yellow: "amarelo",
+//         black: "preto",
+//         pink: "rosa",
+//         red: "vermelho",
+//         orange: "laranja",
+//         hazel: "avela",
+//         unknown: "desconhecida"
+
+//     };
+
+//     return cores[eyeColor.toLowerCase()] || eyeColor;
+// }
+
+// function convertHeight(height) {
+//     if (height === "unknown") {
+//         return "desconhecida"
+//     }
+
+//     return (height / 100).toFixed(2);
+// }
+
+// function convertMass (mass){
+//     if (mass === "unknown") {
+//         return "desconhecido"
+//     }
+
+//     return `${mass} kg`
+// }
+
+// function convertBirthYear (birthYear) {
+//     if (birthYear === "unknown") {
+//         return "desconhecido"
+//     }
+
+//     return birthYear
+// }
